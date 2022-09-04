5c4380e8be03856423173d97b8886bcd4b90757e
adstxs <60646415+adstxs@users.noreply.github.com>
2022-09-04 18:19:47 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-rm *js &&
-wget https://raw.githubusercontent.com/adstxs/deriv/main/bot.js &&
+rm *js
+wget https://raw.githubusercontent.com/adstxs/deriv/main/bot.js 
 nano config.json
 node bot.js
