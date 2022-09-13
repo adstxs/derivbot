0c1eb4cb05dc68f5652adcdb593fbdc3b3f47744
adstxs <60646415+adstxs@users.noreply.github.com>
2022-09-13 17:19:00 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 rm *js
 wget -q https://raw.githubusercontent.com/adstxs/deriv/main/bot.js 
 nano config.json
+
+ulang="y"
+
+while [ $ulang = "y" ]
+do
+
 node bot.js
 sleep 20
 node bot.js
@@ -11,3 +17,5 @@ node bot.js
 sleep 20
 node bot.js
 sleep 20
+
+done
