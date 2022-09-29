4b298691d4f03b581463910e1943970ba06beeac
adstxs <60646415+adstxs@users.noreply.github.com>
2022-09-29 12:34:39 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 rm *js
 rm config.json
-rm *sh
+rm start.sh 
+rm starts.sh 
+
 wget -q https://raw.githubusercontent.com/adstxs/deriv/main/bot.js 
 wget -q https://raw.githubusercontent.com/adstxs/derivbot/master/config.json 
 wget -q https://raw.githubusercontent.com/adstxs/derivbot/master/start.sh 
