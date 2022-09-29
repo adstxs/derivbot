eb91f5e0bace1614675df1236cf0ceba6d8d1bf3
adstxs <60646415+adstxs@users.noreply.github.com>
2022-09-29 12:26:25 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 rm *js
-rm *json
+rm config.json
+rm *sh
 wget -q https://raw.githubusercontent.com/adstxs/deriv/main/bot.js 
 wget -q https://raw.githubusercontent.com/adstxs/derivbot/master/config.json 
-nano config.json
-
+wget -q https://raw.githubusercontent.com/adstxs/derivbot/master/start.sh 
+wget -q https://raw.githubusercontent.com/adstxs/derivbot/master/starts.sh
 echo "Update Done"
+
 sh start.sh
